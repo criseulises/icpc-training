c95ea3098d6a2de6e31cf2dd287b0d89a50744d0
Cristian Sánchez <cristian.sanchez@agilesoft.cl>
2025-07-09 00:46:17 -0400

    A and B exercises added

--- ABProblem.cpp
+++ ABProblem.cpp
@@ -3,31 +3,7 @@ using namespace std;
 
 int main()
 {
-    string mainString[] = {"and", "not", "that", "the", "you"};
-    int size;
-    cin >> size;
-    string words[size];
-
-
-    for (int i = 0; i < size; i++)
-    {
-        cin >> words[i];
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < 6; j++)
-        {
-            if(words[i] == mainString[j])
-            {
-                cout << "Yes";
-                return 0;
-            }
-        }
-        
-    }
-    
-    cout << "No";
-
-    return 0;
+    int A, B;
+    cin >> A >> B;
+    cout << A + B; 
 }
--- exercises/B.cpp
+++ exercises/B.cpp
@@ -1,1 +1,22 @@
-``````
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (A % 10 == 7)
+        {
+            cout << "Yes";
+            return 0;
+        }
+        A = A / 10;
+    }
+
+    cout << "No";
+
+    return 0;
+}
